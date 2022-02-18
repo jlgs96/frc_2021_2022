f0ed93facd61e6cdef89bcbb7c03a46fcae14869
jlgs96 <joselgs96@gmail.com>
2022-02-18 12:39:11 +0100

    Commit inicial sesion 0

---
+++ Sesion0/Sesion0.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// ----------- PRACTICAS DE FUNDAMENTOS DE REDES DE COMUNICACIONES -----------
+// ---------------------------- CURSO 2021/22 --------------------------------
+// ----------------------------- SESION1.CPP ---------------------------------
+//============================================================================
+
+
+#include <stdio.h>
+#include <iostream>
+#include "linkLayer.h"
+
+
+
+
+using namespace std;
+
+
+int main()
+{
+ interface_t iface;
+ pcap_if_t *avail_ifaces=NULL;
+ 
+ printf("\n----------------------------\n");
+ printf("------ SESION 1 - FRC ------\n");
+ printf("----------------------------\n");
+    
+ avail_ifaces=GetAvailAdapters(); 
+ printf("----------------\n");
+ setDeviceName(&iface, "lo");
+ printf("----------------\n");
+ GetMACAdapter(&iface);
+ printf("----------------\n");
+
+ return 0;
+}

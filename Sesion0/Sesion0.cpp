d532585fe25352af13feb3afee350b1f3a8476ab
jlgs96 <joselgs96@gmail.com>
2022-02-24 10:53:23 +0100

    Recorrido hecho, intentar no cargarse la lista.

--- Sesion0/Sesion0.cpp
+++ Sesion0/Sesion0.cpp
@@ -21,17 +21,56 @@ int main()
  pcap_if_t *avail_ifaces=NULL;
  
  
- printf("\n holaaajochemiiiii");
  printf("\n----------------------------\n");
  printf("------ SESION 1 - FRC ------\n");
  printf("----------------------------\n");
     
  avail_ifaces=GetAvailAdapters(); 
- printf("----------------\n");
- setDeviceName(&iface, "lo");
- printf("----------------\n");
- GetMACAdapter(&iface);
- printf("----------------\n");
+int index = 0;
+
+int iseleccionada;
+printf("Interfaces disponibles: \n");
+while (avail_ifaces != NULL)
+{
+  
+    cout<< "["<< index << "] ";
+    printf(avail_ifaces->name);
+    printf("\n" );
+    index++;
+    avail_ifaces = avail_ifaces->next;
+    
+
+}
+
+printf("Seleccione interfaz : ");
+
+cin >> iseleccionada;
+
+
+while (avail_ifaces != NULL)
+{
+    if (iseleccionada == index)
+    {
+        printf("Interfaz elegida: ");
+        printf(avail_ifaces->name);
+        setDeviceName(&iface, avail_ifaces->name);
+        GetMACAdapter(&iface);
+        printf("La dirección MAC es: ");
+        PrintMACAdapter(&iface);
+    }else
+        avail_ifaces = avail_ifaces->next;
+        index++;
+    
+}
+
+
+/*printf("Interfaz elegida: ");
+printf(iface.deviceName);
+cout<<iface.deviceName<<endl;
+GetMACAdapter(&iface);
+printf("La MAC es: ");
+cout << std::hex << (int) iface.MACaddr << endl;
+ */
 
  return 0;
 }
